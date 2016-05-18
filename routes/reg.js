4e4f5884919d631b878a52a4d97576e0e72206cf
yichengjie <626659321@qq.com>
2016-05-18 17:40:42 +0800

    app正常运行保存进数据

--- routes/reg.js
+++ routes/reg.js
@@ -16,32 +16,42 @@ router.post('/', function(req, res) {
     var password = req.body.password ;
     var email = req.body.email ;
     var password_repeat = req.body.password_repeat ;
+    console.info('用户注册') ;
     if(password != password_repeat){
+        console.info('两次输入的密码不一致!') ;
         req.flash('error','两次输入的密码不一致!') ;
         return res.redirect('/reg') ;
     }
-    var md5 = crypto.createHash('md5') ;
-    password= md5.update(req,body.password).digest('hex') ;
+
+
+    //var md5 = crypto.createHash('md5') ;
+    //password= md5.update(req,password).digest('hex') ;
     var newUser = new User({
         name:name,
         password:password,
         email:email
     }) ;
 
+    console.info(JSON.stringify(newUser)) ;
+
     User.get(newUser.name, function (err,user) {
         if(err){
             req.flash('error',err) ;
+            console.info(err) ;
             return res.redirect('/') ;
         }
         if(user){
             req.flash('error','用户名已存在!') ;
+            console.info('用户名已存在!') ;
             return res.redirect('/reg') ;
         }
         newUser.save(function(err,user){
             if(err){
                 req.flash('error',err) ;
+                console.info(err) ;
                 return res.redirect('/reg') ;
             }
+            console.info('注册成功!') ;
             req.session.user = user ;
             req.flash('success','注册成功！') ;
             res.redirect('/') ;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user') ;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('respond with a resource');
 });
 
 router.get('/detail', function(req, res, next) {
